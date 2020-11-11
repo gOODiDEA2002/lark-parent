7a99b16326b409cc2734457e655106d754fb2960
andy.yuan <goodidea2002@gmail.com>
2020-11-11 15:42:22 +0800

    lark-api增加Swagger的自动配置

---
+++ lark-api/src/main/java/lark/api/spring/SpringFoxConfig.java
@@ -0,0 +1,26 @@
+package lark.api.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ *
+ * @author Andy Yuan
+ * @date 2020/11/1
+ */
+@Configuration
+public class SpringFoxConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
