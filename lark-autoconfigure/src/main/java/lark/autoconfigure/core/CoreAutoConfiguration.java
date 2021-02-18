25eabeac5b634fca11ce9b55d54a079419a58bc1
andy.yuan <goodidea2002@gmail.com>
2021-02-18 18:23:57 +0800

    update: 移除不需要的项目

--- lark-autoconfigure/src/main/java/lark/autoconfigure/core/CoreAutoConfiguration.java
+++ lark-autoconfigure/src/main/java/lark/autoconfigure/core/CoreAutoConfiguration.java
@@ -1,15 +1,6 @@
 package lark.autoconfigure.core;
 
-import lark.db.DatabaseService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * @author cuigh
--- lark-util/src/main/java/lark/util/lock/ProcessingException.java
+++
@@ -1,8 +0,0 @@
-package lark.util.lock;
-
-/**
- * Created by Andy Yuan on 2020/11/3.
- */
-public class ProcessingException extends RuntimeException {
-
-}
